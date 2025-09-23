835ae07bd1592aabb2c9bada3a544944292c61c3
Johnnythetony <jamp1998@gmail.com>
2025-09-23 13:36:39 +0200

    Añadir funcionalidad screenshot para navegadores

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,8 +1,9 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Platform } from "react-native";
 import { useEffect, useState, useRef } from 'react';
 import { ImageSourcePropType } from "react-native";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { captureRef } from 'react-native-view-shot';
+import domtoimage from 'dom-to-image';
 
 import * as MediaLibrary from 'expo-media-library';
 import Button from '@/components/Button';
@@ -58,6 +59,7 @@ const Index = () => {
   };
 
   const onSaveImageAsync = async () => {
+    if (Platform.OS !== 'web') {
     try {
       const localUri = await captureRef(imageRef, {
         height: 440,
@@ -70,6 +72,22 @@ const Index = () => {
       }
     } catch (e) {
       console.log(e);
+    }
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        let link = document.createElement('a');
+        link.download = 'sticker-smash.jpeg';
+        link.href = dataUrl;
+        link.click();
+      } catch (e) {
+        console.log(e);
+      }
     }
   };
 
